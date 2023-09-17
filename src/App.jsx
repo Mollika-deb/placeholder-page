9697c538f05cc3672dceb75cdab5d0ed0a88c05a
Mollika-Deb <mollika1551@gmail.com>
2023-09-17 23:52:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* Coming Soon: The Educator - Where Excellence Awaits!" */}
+      <h1 className="font-bold text-red-800">jhs</h1>
+    </>
+  );
+}
+
+export default App;
